d71ebf71bb39af5396829b714dfb0950754c5659
Lio Schimanko <lsb271197@gmail.com>
2026-07-17 12:02:30 -0300

    chore: prepare valence package

--- src/main.js
+++ src/main.js
@@ -1,38 +1,46 @@
-// src/main.js
-
 // src/main.js in the 'valence' project
 
 // Import the foundational styles so they are bundled with the package
-import '.stories/sub-atomic/index.css'; 
+import './stories/sub-atomic/index.css';
 
 // Sub-atomic
-export * from './stories/sub-atomic/Icons/Icon.js';
+export * from './stories/sub-atomic/Iconography/Iconography.js';
 
 // Atoms
 export * from './stories/atoms/Button/Button.js';
 export * from './stories/atoms/Check/Check.js';
 export * from './stories/atoms/Divider/Divider.js';
-export * from './stories/atoms/FontSizeToggle/FontSizeToggle.js';
-export * from './stories/atoms/Kbd/Kbd.js';
+export * from './stories/atoms/Icon/Iconography.js';
+export * from './stories/atoms/KBD/Kbd.js';
 export * from './stories/atoms/Loader/Loader.js';
-export * from './stories/atoms/ModeToggle/ModeToggle.js';
 export * from './stories/atoms/Pill/Pill.js';
+export * from './stories/atoms/Radio/Radio.js';
+export * from './stories/atoms/SeekBar/SeekBar.js';
 export * from './stories/atoms/SliderDot/SliderDot.js';
 export * from './stories/atoms/Subtitle/Subtitle.js';
 export * from './stories/atoms/Tab/Tab.js';
+export * from './stories/atoms/Thumbnail/Thumbnail.js';
 export * from './stories/atoms/Toggle/Toggle.js';
-export * from './stories/atoms/Tooltip/Tooltip.js';
 
 // Molecules
-export * from './stories/molecules/AudioProgress/AudioProgress.js';
+export * from './stories/molecules/AudioPlayer/AudioPlayer.js';
 export * from './stories/molecules/Breadcrumb/Breadcrumb.js';
+export * from './stories/molecules/ComboCard/ComboCard.js';
 export * from './stories/molecules/ComboTabs/ComboTabs.js';
+export * from './stories/molecules/GalleryItem/GalleryItem.js';
 export * from './stories/molecules/ItemRow/ItemRow.js';
 export * from './stories/molecules/MetricCard/MetricCard.js';
 export * from './stories/molecules/SearchInput/SearchInput.js';
+export * from './stories/molecules/SliderDot/SliderDots.js';
+export * from './stories/molecules/Summary/Summary.js';
+export * from './stories/molecules/Toast/Toast.js';
+export * from './stories/molecules/Tooltip/Tooltip.js';
 export * from './stories/molecules/VideoControls/VideoControls.js';
 
 // Organisms
-export * from './stories/organisms/AiSummary/AiSummary.js';
-export * from './stories/organisms/GalleryItem/GalleryItem.js';
+export * from './stories/organisms/CaseSlide/CaseSlide.js';
+export * from './stories/organisms/ContextualMenu/ContextualMenu.js';
+export * from './stories/organisms/Gallery/Gallery.js';
+export * from './stories/organisms/Header/Header.js';
 export * from './stories/organisms/Modal/Modal.js';
+export * from './stories/organisms/VideoPlayer/VideoPlayer.js';
